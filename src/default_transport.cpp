8833fbda9793d68ef2d61ad277248cb70750542d
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2024-05-04 23:57:00 +0900

    Update default_transport.cpp

--- src/default_transport.cpp
+++ src/default_transport.cpp
@@ -4,7 +4,11 @@ extern "C"
 {
   #include <stdio.h>
   #include <stdbool.h>
+#if defined(ARDUINO_ARCH_RENESAS)
+  #include <time.h>
+#else
   #include <sys/time.h>
+#endif
 
   int clock_gettime(clockid_t unused, struct timespec *tp) __attribute__ ((weak));
   bool arduino_transport_open(struct uxrCustomTransport * transport) __attribute__ ((weak));
